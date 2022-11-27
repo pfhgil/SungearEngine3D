06a6ae542f63751b989654aaa49c15cbf51c13f7
pfhgil <stukalovila76@gmail.com>
2022-11-27 19:40:35 +0300

    Small add

---
+++ Core3D/include/Core3D/GameObject/GameObject.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace Core3D
+{
+	namespace GameObject
+	{
+		class GameObject
+		{
+			private:
+				vector<Component> _components;
+
+			public:
+				GameObject() { }
+				~GameObject() { }
+				
+		};
+
+		class Component
+		{
+			public:
+				Component() { }
+				~Component() { }
+
+				GameObject* _gameObject;
+
+				void Init();
+				void Update();
+				void DeltaUpdate(float deltaTime);
+		};
+
+		class MeshRendererComponent : Component
+		{
+			public:
+				MeshRendereComponent() { }
+				~MeshRendereComponent() { }
+				~MeshRendereComponent() { }
+		};
+	}
+}
--- SungearEngine3D/SungearEngine3D.cpp
+++ SungearEngine3D/SungearEngine3D.cpp
@@ -24,18 +24,29 @@ static const struct
     float r, g, b;
 } vertices[8] =
 {
-    {  -1.0, -1.0,  1.0, 1.f, 0.f, 0.0f },
-    {  1.0, -1.0,  1.0, 1.f, 0.f, 0.0f },
-    {  -1.0,  1.0,  1.0, 1.f, 0.f, 0.0f },
-    {  1.0,  1.0,  1.0, 1.f, 0.f, 0.0f },
-    {  -1.0, -1.0, -1.0, 1.f, 0.f, 0.0f },
-    {  1.0, -1.0, -1.0, 1.f, 0.f, 0.0f },
-    {  -1.0,  1.0, -1.0, 1.f, 0.f, 0.0f },
-    {  1.0,  1.0, -1.0, 1.f, 0.f, 0.0f }
+    {  -1, +1, -1, 1.f, 0.f, 0.0f },
+    {  +1, +1, -1, 1.f, 1.f, 0.0f },
+    {  +1, -1, -1, 1.f, 0.f, 1.0f },
+    {  -1, -1, -1, 0.f, 1.f, 0.0f },
+    {  -1, +1, +1, 0.f, 0.f, 1.0f },
+    {  +1, +1, +1, 1.f, 1.f, 1.0f },
+    {  +1, -1, +1, 0.2f, 0.f, 0.7f },
+    {  -1, -1, +1, 0.7f, 0.5f, 0.2f }
 };
 
 static const GLushort indices[] = {
-    0, 1, 2, 3, 7, 1, 5, 4, 7, 6, 2, 4, 0, 1
+    0, 1, 2,
+    0, 2, 3,
+    2, 1, 5,
+    2, 5, 6,
+    3, 2, 6,
+    3, 6, 7,
+    0, 3, 7,
+    0, 7, 4,
+    1, 0, 4,
+    1, 4, 5,
+    6, 5, 4,
+    6, 4, 7,
 };
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
@@ -73,11 +84,13 @@ int main(void)
     // NOTE: OpenGL error checks have been omitted for brevity
 
     GLBuffer* vertex_buffer = new GLC::GLBuffer();
-    vertex_buffer->Bind()->Data(sizeof(vertices), vertices);//Биндим буффер и записываем в него данные
-
-
     GLBuffer* index_buffer = new GLC::GLBuffer(GL_ELEMENT_ARRAY_BUFFER);
-    index_buffer->Bind()->Data(sizeof(indices), indices);//Биндим буффер и записываем в него данные
+
+    vertex_buffer->Bind();
+    vertex_buffer->Data(sizeof(vertices), vertices);//Биндим буффер и записываем в него данные
+
+    index_buffer->Bind();
+    index_buffer->Data(sizeof(indices), indices);//Биндим буффер и записываем в него данные
 
     string shaderText = FileUtils::ReadAllFile("D:/Pixelfield/C++Learning/SungearEngine3D/Core3D/Resources/Shaders/default.glsl");
     GLShader* shader = (new GLShader(shaderText))->Use();
@@ -99,6 +112,8 @@ int main(void)
     //m = translate(m, vec3(0.0f, 0.0f, 0.0f));
     v = translate(v, vec3(0.0f, 0.0f, -10.0f));
 
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
@@ -108,15 +123,15 @@ int main(void)
         ratio = width / (float)height;
 
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        m = rotate(mat4(1.0f), (float) glfwGetTime(), vec3(1.0f, 0.0f, 0.0f));
+        m = rotate(mat4(1.0f), (float) glfwGetTime(), vec3(1.0f, 1.0f, 0.0f));
         p = perspective(45.0f, ratio, 0.1f, 100.0f);
         mvp = p * v * m;
 
         shader->Use();
         shader->uMat4x4("MVP", mvp);
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_SHORT, nullptr);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_SHORT, nullptr);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
